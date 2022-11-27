8447b979a59715b9d5b43c2060fa3ec5e0fb2e5e
TTania <116368430+TTania-1@users.noreply.github.com>
2022-11-27 15:27:18 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+#A back slash followed by the character n  \n means that the output should move to a new line before displaying the next sequence of characters. 
+
+# If we want to display a back slash we need to use \\ when we want to display an actual back slash.
+
+print("\n Displays a new line")
+print("\t Displays a tab space")
+print("\\ Displays a back slash")
+print("\" Displays a double quote")
+print("\' Displays a single quote")
+
+# A message is displayed
+print(" \n\t\t\"I am Beep!\" ")
